fb02bf1eed3431f2289f85eaa5b74d62752cb30a
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-07-17 14:06:40 +0400

    Functions

--- src/scripts/notes.js
+++ src/scripts/notes.js
@@ -86,3 +86,64 @@
 // console.log(y, y1, y2);
 // y - ograniczone przez funkcję
 // y1 i y2 - powoduje błąd - bo jest ograniczone klamerkami
+
+
+// 2. Funkcje - służa do reużywalności kodu:
+// named i anonymous - nie mają nazwy co nie przeczy zasadzie reużywalności, przez to że jest odwołanie zwrotne CB (Call Back)- coś co wywyła się kiedy będzie potzrebne
+
+//named function
+function addDigits (digit1, digit2) {
+    return digit1 + digit 2;
+}
+
+addDigits(1,2);
+
+//anonymous function
+//classic
+// function expression
+function (difit1, digit2) {
+   return digit1 + digit2;
+}
+//nie można wywołać te funcji, bo nie ma identyfikatora i nie jest call backiem
+
+// function expression (wyrażenie funkcujne) - przypisanie deklaracji funkcji do zmiennje
+addDigits2(1,2);
+
+const addDigits2= function (difit1, digit2) {
+    return digit1 + digit2;
+}
+
+addDigits2(1,2);
+
+//arrow function - funkcja strzałkowa, fat arrow function (nie ma this, ani arguments)
+(digit1, digit2) => {
+  return digit1 + digit2;
+}
+
+// nie ma identyfikatora wiec nie da się tego wywowołac wiec robimy wyrażenie funkcjyjne
+const addDigits3 = (digit1, digit2) => {
+    return digit1 + digit2;
+}
+addDigits3(1,2);
+
+// wariacje arrow function, parametry:
+// - nie ma parametrów:
+const add = () => { return 42;}
+// - jeden parametr:
+const add1 = a => { return a + 42;}
+// nie pisać tak, zawsze pisać okrągłe nawiasy
+// - więcej parametrów:
+const add2 = (a, b) => { return a + b;}
+// - implicit return - jak nie ma klamerek, to automatycznie zwraca wynik z wyrażenia:
+const add3 = (a, b) => a + b;
+
+// przykłady
+// 1  przykład a mniejsze równe zero: (a) => (a <= 0), (a) => { return a <= 0}
+const magic = a => a <= 0;
+
+// 2 przykład
+const magic2 = () => {
+    return {a: 42}
+}
+// bez nawiasow bład:
+const magic3 = () => ({a: 42})
